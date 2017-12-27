b444d6a33772e66a45d0d4ef0e67c3f00c975b13
Philihp Busby <philihp@gmail.com>
2017-12-26 23:31:23 -0800

    deploy to gh-pages

--- src/components/tool_track.js
+++ src/components/tool_track.js
@@ -21,7 +21,7 @@ export default class ToolTrack extends React.Component {
           <tr>
             {this.props.values.map((v,i) =>
               <td key={i}>
-                <svg width="30" height="25" viewBox="0 0 100 90" xmlns="http://www.w3.org/2000/svg">
+                <svg width="25" height="25" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
                 {i === this.props.track[0] && (
                   <g>
                     <path d="M 3 45 V 65 A 47 20 0 0 0 50 85 A 47 20 0 0 0 97 65 V 45"
