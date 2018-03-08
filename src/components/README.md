f541f7a505a77e0458bbaf96f6344084391301d5
Philihp Busby <philihp@gmail.com>
2018-03-08 09:47:01 -0800

    make wool weaver options selectable

---
+++ src/components/README.md
@@ -0,0 +1,15 @@
+Board
+  ActionOptions - usually hidden
+    some component from ./action_options
+  Tableau red
+    TableauLand
+  GlobalStatus
+    CurrentPlayer
+    LighthouseStatus
+  RoundBoard
+  PreparationsBoard
+  ActionsBoard
+    WorkerSpot...
+    ToolTrack...
+  MoveSelect
+  Tableau yellow
--- src/components/action_options/wool_weaver.js
+++ src/components/action_options/wool_weaver.js
@@ -2,32 +2,46 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { toolValue } from '../../game/tool_increments'
 
-const weavingLooms = 'weavingLooms'
-
 //toolValue('weavingLooms', G.tools)
 
-// TODO need a form and action for this
-const WoolWeaver = ({ G, ctx, moves }) => {
-  const wool = G.players[ctx.currentPlayer].goods.wool
-  const looms = toolValue(
-    weavingLooms,
-    G.toolSpaces[weavingLooms][ctx.currentPlayer]
-  )
-  const maxTimes = Math.min(wool, looms)
-  return (
-    <div>
-      Convert a wool to woolen, per loom<br />
-      <br />
-      {Array.from(Array(maxTimes + 1), (v, idx) => idx).map(i => {
-        return (
-          <div key={i}>
-            <input type="radio" name="woolWeaverConvert" id={i} />
-            <label for={i}>Convert {i} wool</label>
-          </div>
-        )
-      })}
-    </div>
-  )
+class WoolWeaver extends React.Component {
+  constructor(props) {
+    super(props)
+    const { G, ctx } = props
+    const weavingLooms = 'weavingLooms'
+    const wool = G.players[ctx.currentPlayer].goods.wool
+    const looms = toolValue(
+      weavingLooms,
+      G.toolSpaces[weavingLooms][ctx.currentPlayer]
+    )
+    const maxTimes = Math.min(wool, looms)
+    this.state = {
+      maxTimes,
+      defaultChecked: maxTimes,
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        Convert a wool to woolen, per loom<br />
+        <br />
+        {Array.from(Array(this.state.maxTimes + 1), (v, idx) => idx).map(i => {
+          return (
+            <div key={i}>
+              <input
+                type="radio"
+                name="woolWeaverConvert"
+                id={i}
+                checked={this.state.defaultChecked}
+              />
+              <label htmlFor={i}>Convert {i} wool</label>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
 }
 
 WoolWeaver.propTypes = {
