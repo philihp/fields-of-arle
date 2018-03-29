eda366ffb995c396475daf4490d45d116eaef2dc
Philihp Busby <philihp@gmail.com>
2018-03-28 22:25:02 -1000

    [tableau] reorganized order

--- src/components/tableau.js
+++ src/components/tableau.js
@@ -5,19 +5,19 @@ import './tableau.css'
 
 const Tableau = ({ player }) => (
   <div className="Tableau">
+    <TableauFarm land={player.land} dikes={player.dikes} />
     <div className="Tokens">
       <div>
-        Floating:
+        {player.tokens.length > 0 && 'Limbo: '}
         <span style={{ color: 'red', fontWeight: 'bold' }}>
           {player.tokens.join(', ')}
         </span>
       </div>
       <div>Inventory: {player.inventory.join(', ')}</div>
     </div>
-    <div className="FloatingInventory" />
-    <TableauFarm land={player.land} dikes={player.dikes} />
+    <hr />
     <div className="Goods">
-      <div> Food: {player.goods.food}</div>
+      <div>Food: {player.goods.food}</div>
       <div>Wool: {player.goods.wool}</div>
       <div>Flax: {player.goods.flax}</div>
       <div>Hides: {player.goods.hides}</div>
