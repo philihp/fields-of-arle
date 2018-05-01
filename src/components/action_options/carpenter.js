c34cd0ba67748be89a2a96d6f4d04fc053a39a97
Philihp Busby <philihp@gmail.com>
2018-05-01 12:56:45 -0400

    [carpenter] give player build option

--- src/components/action_options/carpenter.js
+++ src/components/action_options/carpenter.js
@@ -7,25 +7,93 @@ const affordStall = player =>
   player.goods.grain >= 1 && afford(player.inventory, ['clay', 'clay'])
 const affordStable = player => afford(player.inventory, ['brick', 'brick'])
 
-const Carpenter = ({ G, ctx, moves }) => {
-  const handleBuild = (row, col) => e => {
-    moves.option({ row, col })
+class Carpenter extends React.Component {
+  constructor(props) {
+    super()
+    const { G, ctx, moves } = props
+
+    this.state = {
+      mode: null,
+    }
   }
-  const player = G.players[ctx.currentPlayer]
-  return (
-    <TableauFarm
-      land={player.land}
-      dikes={player.dikes}
-      handleBuildStall={
-        affordStall(player) && G.supplies.stallDepot > 0 ? handleBuild : null
-      }
-      handleBuildStable={
-        affordStable(player) && G.supplies.stableDoubleStall > 0
-          ? handleBuild
-          : null
-      }
-    />
-  )
+
+  handleButton = mode => e => {
+    this.setState({ mode })
+  }
+
+  handleBuild = (row, col) => e => {
+    this.props.moves.option({ row, col })
+  }
+
+  render() {
+    const { G, ctx, moves } = this.props
+    const player = G.players[ctx.currentPlayer]
+    switch (this.state.mode) {
+      case 'stall':
+        return (
+          <div>
+            <button onClick={this.handleButton(null)}>Back</button>
+            <TableauFarm
+              land={player.land}
+              dikes={player.dikes}
+              handleBuildStall={
+                affordStall(player) && G.supplies.stallDepot > 0
+                  ? this.handleBuild
+                  : null
+              }
+              handleBuildStable={
+                affordStable(player) && G.supplies.stableDoubleStall > 0
+                  ? this.handleBuild
+                  : null
+              }
+            />
+          </div>
+        )
+      case 'stable':
+        return (
+          <div>
+            <button onClick={this.handleButton(null)}>Back</button>
+            <TableauFarm
+              land={player.land}
+              dikes={player.dikes}
+              handleBuildStall={
+                affordStall(player) && G.supplies.stallDepot > 0
+                  ? this.handleBuild
+                  : null
+              }
+              handleBuildStable={
+                affordStable(player) && G.supplies.stableDoubleStall > 0
+                  ? this.handleBuild
+                  : null
+              }
+            />
+          </div>
+        )
+      case 'building':
+        return <div>building</div>
+        break
+      default:
+        return (
+          <div>
+            <button
+              disabled={!affordStall(player)}
+              onClick={this.handleButton('stall')}
+            >
+              Build Stall
+            </button>
+            <button
+              disabled={!affordStable(player)}
+              onClick={this.handleButton('stable')}
+            >
+              Build Stable
+            </button>
+            <button onClick={this.handleButton('building')}>
+              Build Building
+            </button>
+          </div>
+        )
+    }
+  }
 }
 
 Carpenter.propTypes = {
