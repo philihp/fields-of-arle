f1f91e60c5b5457e5db4291d75a8e336bdf94d6d
Philihp Busby <philihp@gmail.com>
2018-10-01 11:18:30 -1000

    [building] fix passing criteria

--- src/game/building/workshop.js
+++ src/game/building/workshop.js
@@ -14,8 +14,8 @@ export const resetPassedIfWorkshops = (G, ctx) => {
     1: playersWorkshops(G.players['1']),
   }
   const passed = {
-    0: unusedWorkshops['0'].length > 0,
-    1: unusedWorkshops['1'].length > 0,
+    0: unusedWorkshops['0'].length === 0,
+    1: unusedWorkshops['1'].length === 0,
   }
   return { ...G, passed, unusedWorkshops }
 }
