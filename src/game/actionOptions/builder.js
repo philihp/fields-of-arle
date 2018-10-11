29cd41458e4bedba56bfc73433630c8f41cc769a
Philihp Busby <philihp@gmail.com>
2018-10-11 00:07:11 -0700

    [building] break apart the getting of the building

--- src/game/actionOptions/builder.js
+++ src/game/actionOptions/builder.js
@@ -1,51 +1,78 @@
 import { compose } from 'redux'
 import { spendInventory, spendGoods } from '../../game/common'
+import { actionOption } from '../common/player'
 
-// these are the steps that must be composed
-// and executed in reverse order on { G, ctx, ...args }
+const clearSelected = ({ G, ...args }) => ({
+  G: {
+    ...G,
+    selected: undefined,
+  },
+  ...args,
+})
 
-// setActionNull
-// clearSelected
-// takeBenefitFromBuilding
-// removeBuilding({building})
-// placeBuilding({building, row, col})
-// spendInventory(lookupInventoryCost(building, cost)
-// spendGoods(lookupGoodsCost(building, cost))
+const removeBuilding = building => ({ G, ...args }) => ({
+  G: {
+    ...G,
+    buildings: G.buildings.map(b => (b === building ? null : b)),
+  },
+  ...args,
+})
 
-const all = ({ G, ctx, selected, ...args }) => {
-  const player = G.players[ctx.currentPlayer]
-  const { row, col, building, cost } = selected
-  return {
-    G: {
-      ...G,
-      action: null,
-      selected: undefined,
-      buildings: G.buildings.map(b => (b === building ? null : b)),
-      players: {
-        ...G.players,
-        [ctx.currentPlayer]: {
-          ...player,
-          inventory: spendInventory(player.inventory, cost),
-          goods: spendGoods(player.goods, cost),
-          land: [
-            ...player.land.slice(0, row),
-            [
-              ...player.land[row].slice(0, col),
-              {
-                ...player.land[row][col],
-                type: building,
-              },
-              ...player.land[row].slice(col + 1),
-            ],
-            ...player.land.slice(row + 1),
+const placeBuilding = ({ building, row, col }) => ({ G, ctx, ...args }) => ({
+  G: {
+    ...G,
+    players: {
+      ...G.players,
+      [ctx.currentPlayer]: {
+        ...G.players[ctx.currentPlayer],
+        land: [
+          ...player.land.slice(0, row),
+          [
+            ...player.land[row].slice(0, col),
+            {
+              ...player.land[row][col],
+              type: building,
+            },
+            ...player.land[row].slice(col + 1),
           ],
-        },
+          ...player.land.slice(row + 1),
+        ],
       },
     },
-    ctx,
-    ...args,
-  }
-}
+  },
+  ctx,
+  ...args,
+})
+
+const expendInventory = ({ cost }) => ({ G, ctx, ...args }) => ({
+  G: {
+    ...G,
+    players: {
+      ...G.players,
+      [ctx.currentPlayer]: {
+        ...G.players[ctx.currentPlayer],
+        inventory: spendInventory(G.players[ctx.currentPlayer].inventory, cost),
+      },
+    },
+  },
+  ctx,
+  ...args,
+})
+
+const expendGoods = ({ cost }) => ({ G, ctx, ...args }) => ({
+  G: {
+    ...G,
+    players: {
+      ...G.players,
+      [ctx.currentPlayer]: {
+        ...G.players[ctx.currentPlayer],
+        goods: spendGoods(G.players[ctx.currentPlayer].goods, cost),
+      },
+    },
+  },
+  ctx,
+  ...args,
+})
 
 export default ({ G, ctx, ...args }) => {
   const {
@@ -61,7 +88,14 @@ export default ({ G, ctx, ...args }) => {
     selected.hasOwnProperty('row') &&
     selected.hasOwnProperty('cost')
   ) {
-    return compose(all)({ G, ctx, selected, ...args }).G
+    return compose(
+      actionOption(null),
+      clearSelected,
+      removeBuilding(building),
+      placeBuilding(selected),
+      expendInventory(cost),
+      expendGoods(cost)
+    )({ G, ctx, selected, ...args }).G
   } else {
     return {
       ...G,
