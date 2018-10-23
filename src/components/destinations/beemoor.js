ad6933d3a6abbeb6a405818e1406fd7ded582483
Philihp Busby <philihp@gmail.com>
2018-10-23 01:26:33 -0700

    [load] travel to beemoor and sell peat

--- src/components/destinations/beemoor.js
+++ src/components/destinations/beemoor.js
@@ -1,9 +1,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Beemoor = ({ inventory, handleSellAtDestination }) => <div>Beemoor</div>
+class Beemoor extends React.Component {
+  constructor(props) {
+    super()
+  }
+
+  render() {
+    const { inventory, handleSellAtDestination } = this.props
+    return (
+      <div>
+        <br />
+        <b>Sellers at Beemoor will buy:</b>
+        <ul>
+          <li>
+            <button
+              disabled={
+                this.props.conversionInputs[0] || !inventory.includes('peat')
+              }
+              type="button"
+              onClick={handleSellAtDestination(0, 'peat')}
+            >
+              Peat
+            </button>
+            for 2 food
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
 
 Beemoor.propTypes = {
+  conversionInputs: PropTypes.array.isRequired,
   inventory: PropTypes.array.isRequired,
   handleSellAtDestination: PropTypes.func.isRequired,
 }
--- src/game/destinations/beemoor.js
+++ src/game/destinations/beemoor.js
@@ -1,3 +1,20 @@
+import { compose } from 'redux'
+import { identity } from '../common/index'
+import {
+  applyToCurrentPlayer,
+  curriedAddGoodsToPlayer,
+  inventorySpendFromPlayer,
+} from '../common/player'
+
 export const size = 1
 
-export default conversionInputs => applyToCurrentPlayer(compose())
+const sellPeat = input => {
+  if (input === null) return identity
+  return compose(
+    inventorySpendFromPlayer('peat'),
+    curriedAddGoodsToPlayer('food', 2)
+  )
+}
+
+export default conversionInputs =>
+  applyToCurrentPlayer(sellPeat(conversionInputs[0]))
