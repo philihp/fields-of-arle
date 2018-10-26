39510569eda6e9c97ef1ce8500f2f7b53043b073
Philihp Busby <philihp@gmail.com>
2018-10-25 22:32:56 -0700

    [load] display options at hage

--- src/components/destinations/hage.js
+++ src/components/destinations/hage.js
@@ -1,9 +1,46 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Hage = ({ inventory, handleSellAtDestination }) => <div>Hage</div>
+const isField = s => s.match(/^(flax-|grain-)/)
+
+class Hage extends React.Component {
+  constructor(props) {
+    super()
+  }
+
+  render() {
+    const { inventory, handleSellAtDestination } = this.props
+    const fields = inventory.filter(isField)
+    return (
+      <div>
+        <b>Sellers at Hage will buy:</b>
+        <ul>
+          <li>
+            {fields.length !== 0 &&
+              fields.map(field => (
+                <button
+                  disabled={this.props.conversionInputs[0]}
+                  type="button"
+                  onClick={handleSellAtDestination(0, field)}
+                >
+                  {field}
+                </button>
+              ))}
+            {fields.length === 0 && (
+              <button disabled type="button">
+                Field
+              </button>
+            )}
+            for 2 food
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
 
 Hage.propTypes = {
+  conversionInputs: PropTypes.array.isRequired,
   inventory: PropTypes.array.isRequired,
   handleSellAtDestination: PropTypes.func.isRequired,
 }
