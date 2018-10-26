5bdf81a0722316d07c3d57844ab7e84ef9e7f3c1
Philihp Busby <philihp@gmail.com>
2018-10-26 01:35:38 -0700

    [load] fix imports on server side

--- src/game/destinations/hage.js
+++ src/game/destinations/hage.js
@@ -1,3 +1,6 @@
+import { compose } from 'redux'
+import { applyToCurrentPlayer } from '../common/player'
+
 export const size = 1
 
 export default conversionInputs => applyToCurrentPlayer(compose())
