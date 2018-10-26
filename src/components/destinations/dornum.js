a89b60d96f421950ff680654755f30bae38d3ca8
Philihp Busby <philihp@gmail.com>
2018-10-25 22:07:06 -0700

    [load] display options at dornum

--- src/components/destinations/dornum.js
+++ src/components/destinations/dornum.js
@@ -1,9 +1,39 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Dornum = ({ inventory, handleSellAtDestination }) => <div>Dornum</div>
+class Dornum extends React.Component {
+  constructor(props) {
+    super()
+  }
+
+  render() {
+    const { inventory, handleSellAtDestination } = this.props
+    return (
+      <div>
+        {JSON.stringify(inventory)}
+        <br />
+        <b>Sellers at Dornum will buy (pick at least one):</b>
+        <ul>
+          <li>
+            <button
+              disabled={
+                this.props.conversionInputs[0] || !inventory.includes('plow')
+              }
+              type="button"
+              onClick={handleSellAtDestination(0, 'plow')}
+            >
+              Plow
+            </button>
+            for 5 food
+          </li>
+        </ul>
+      </div>
+    )
+  }
+}
 
 Dornum.propTypes = {
+  conversionInputs: PropTypes.array.isRequired,
   inventory: PropTypes.array.isRequired,
   handleSellAtDestination: PropTypes.func.isRequired,
 }
