5734be4cedbadfe6dfb256c701b1ce8428e9c8ec
Philihp Busby <philihp@gmail.com>
2018-10-27 14:38:45 -0700

    [load] create tests for aurich processing

---
+++ src/game/common/__tests__/animals.test.js
@@ -0,0 +1,1 @@
+import { findFirstAnimal } from '../animals'
---
+++ src/game/destinations/__tests__/aurich.test.js
@@ -0,0 +1,97 @@
+import aurich, { size } from '../aurich'
+import { inventoryAddToPlayer, countAnimals } from '../../common/player'
+
+const ctx = { currentPlayer: 0 }
+const G = {
+  players: {
+    0: {
+      // hypothetical 1x1 farm
+      land: [[{ type: 'empty', contents: ['horse'] }]],
+      dikes: [[{ contents: ['sheep', 'horse'] }]],
+      inventory: ['leather'],
+      goods: {
+        food: 0,
+      },
+    },
+  },
+}
+
+describe('aurich load', () => {
+  it('has a size', () => {
+    expect(size).toEqual(2)
+  })
+
+  it('noop', () => {
+    expect(aurich([null, null, null])({ G, ctx })).toEqual({ G, ctx })
+  })
+
+  it('sells leather for 4 food', () => {
+    const player = aurich([['leather'], null, null])({ G, ctx }).G.players['0']
+    expect(player.inventory).not.toContain('leather')
+    expect(player.goods.food).toBe(4)
+  })
+
+  it('sells only one leather for 4 food', () => {
+    const G2 = {
+      ...G,
+      players: {
+        ...G.players,
+        0: inventoryAddToPlayer('leather')(G.players['0']),
+      },
+    }
+    const player = aurich([['leather'], null, null])({ G: G2, ctx }).G.players[
+      '0'
+    ]
+    expect(player.inventory).toContain('leather')
+    expect(player.goods.food).toBe(4)
+  })
+
+  it('sells sheep for 4 food', () => {
+    const player = aurich([null, ['sheep'], null])({ G, ctx }).G.players['0']
+    expect(countAnimals(player)).toEqual({ cattle: 0, horses: 2, sheep: 0 })
+    expect(player.goods.food).toBe(4)
+  })
+
+  it('sells horse in second slot for 4 food', () => {
+    const player = aurich([null, ['horse'], null])({ G, ctx }).G.players['0']
+    expect(countAnimals(player)).toEqual({ cattle: 0, horses: 1, sheep: 1 })
+    expect(player.goods.food).toBe(4)
+  })
+
+  it('sells horse in third slot for 5 food', () => {
+    const player = aurich([null, null, ['horse']])({ G, ctx }).G.players['0']
+    expect(countAnimals(player)).toEqual({ cattle: 0, horses: 1, sheep: 1 })
+    expect(player.goods.food).toBe(5)
+  })
+
+  it('sells horses in second and third slot for 9 food', () => {
+    const player = aurich([null, ['sheep'], ['horse']])({ G, ctx }).G.players[
+      '0'
+    ]
+    expect(countAnimals(player)).toEqual({ cattle: 0, horses: 1, sheep: 0 })
+    expect(player.goods.food).toBe(9)
+  })
+
+  it('fdsa', () => {
+    const G2 = {
+      ...G,
+      players: {
+        ...G.players,
+        0: {
+          // hypothetical 1x1 farm
+          land: [[{ type: 'empty', contents: [] }]],
+          dikes: [[{ contents: [] }]],
+          inventory: ['leather'],
+          goods: {
+            food: 0,
+          },
+        },
+      },
+    }
+    // const player = aurich([null, ['horse'], null])({ G: G2, ctx }).G.players[
+    //   '0'
+    // ]
+    // expect(countAnimals(player)).toEqual({ cattle: 1, horses: 0, sheep: 0 })
+    // expect(player.goods.food).toBe(0)
+  })
+})
