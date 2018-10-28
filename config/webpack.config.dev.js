4e30a5e1998bb47328fbf72d23e83c98e9c412cc
doctor-claus <deysouvik107@gmail.com>
2018-10-28 17:14:26 +0530

    Webpack4 prod support upgrade

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -203,6 +203,11 @@ module.exports = {
       // Make sure to add the new loader(s) before the "file" loader.
     ],
   },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
   plugins: [
     // Makes some environment variables available in index.html.
     // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
