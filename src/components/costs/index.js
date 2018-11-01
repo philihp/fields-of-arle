59be086a5e53929f892f38776f89b4b77aa74ef7
Philihp Busby <philihp@gmail.com>
2018-11-01 01:00:24 -0700

    [building] payment options for inns

--- src/components/costs/index.js
+++ src/components/costs/index.js
@@ -1,12 +1,16 @@
 import workshop from './workshop'
 import mill from './mill'
 import weavingMill from './weavingMill'
+import inn from './inn'
 
 export default {
   farmersHouse: workshop,
   novicesHut: workshop,
   plowMakersWorkshop: workshop,
   workshop,
+  pottersInn: inn,
+  farmersInn: inn,
+  junkDealersInn: inn,
 
   mill,
   weavingMill,
