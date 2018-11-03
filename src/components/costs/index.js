bcf239593d4a786de14bfef0bd6f503fdd6fd86a
Philihp Busby <philihp@gmail.com>
2018-11-03 13:27:34 -0700

    [buildings] payment options for all the smallHouses
    Resolves #95
    Resolves #96
    Resolves #97
    Resolves #98
    Resolves #99
    Resolves #100
    Resolves #101
    Resolves #123
    Resolves #90
    Resolves #106
    Resolves #107
    Resolves #108

--- src/components/costs/index.js
+++ src/components/costs/index.js
@@ -1,16 +1,27 @@
-import workshop from './workshop'
+import smallHouse from './smallHouse'
 import mill from './mill'
 import weavingMill from './weavingMill'
 import inn from './inn'
 
 export default {
-  farmersHouse: workshop,
-  novicesHut: workshop,
-  plowMakersWorkshop: workshop,
-  workshop,
+  farmersHouse: smallHouse,
+  novicesHut: smallHouse,
+  plowMakersWorkshop: smallHouse,
+  workshop: smallHouse,
+  weavingParlor: smallHouse,
+  carpentersWorkshop: smallHouse,
+  colonistsHouse: smallHouse,
+  loadingStation: smallHouse,
+  schnappsDistillery: smallHouse,
+  litterStorage: smallHouse,
+  woodTrader: smallHouse,
+
   pottersInn: inn,
   farmersInn: inn,
   junkDealersInn: inn,
+  milkHouseInn: inn,
+  sluiceYardInn: inn,
+  gulfHouseInn: inn,
 
   mill,
   weavingMill,
