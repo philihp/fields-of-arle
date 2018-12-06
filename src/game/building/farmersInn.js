6b12cc35bb58433130a61b703326791d86f08b4c
Stefan Hanke <stefan.hanke@gmx.org>
2018-12-06 21:21:06 +0100

    simplify function export

--- src/game/building/farmersInn.js
+++ src/game/building/farmersInn.js
@@ -1,4 +1,3 @@
-import { compose } from 'redux'
 import { actionOption } from '../common/player'
 
-export default state => compose(actionOption('farmersInn'))(state)
+export default actionOption('farmersInn')
