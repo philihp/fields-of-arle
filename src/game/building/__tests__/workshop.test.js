d26ef9ccaf18dd2321fc82988c4a77aa8dc00708
Philihp Busby <philihp@gmail.com>
2019-01-22 21:39:16 -0800

    [tests] add tests for workshop turn order at any player count

--- src/game/building/__tests__/workshop.test.js
+++ src/game/building/__tests__/workshop.test.js
@@ -1,4 +1,3 @@
-import { isContext } from 'vm'
 import { workshopTurnOrder, resetPassedIfWorkshops } from '../workshop'
 import { initialState } from '../../index'
 
@@ -16,10 +15,25 @@ describe('resetPassedIfWorkshops', () => {
       const output = resetPassedIfWorkshops(state.G, state.ctx)
       expect(output.usedWorkshops).toHaveLength(0)
     })
-    it('used workshops should be empty', () => {
+    it('player without has passed', () => {
       const state = stateWith(1, 0)
       const output = resetPassedIfWorkshops(state.G, state.ctx)
       expect(output.passed['0']).toBeTruthy()
+    })
+    it('player with workshop has not passed', () => {
+      const state = stateWith(1, 0)
+      const G = {
+        ...state.G,
+        players: {
+          ...state.G.players,
+          0: {
+            ...state.G.players['0'],
+            land: [[{ type: 'workshop', contents: [] }]],
+          },
+        },
+      }
+      const output = resetPassedIfWorkshops(G, state.ctx)
+      expect(output.passed['0']).toBeFalsy()
     })
   })
   describe('2 player', () => {
@@ -28,11 +42,63 @@ describe('resetPassedIfWorkshops', () => {
       const output = resetPassedIfWorkshops(state.G, state.ctx)
       expect(output.usedWorkshops).toHaveLength(0)
     })
-    it('used workshops should be empty', () => {
+    it('both players have passed', () => {
       const state = stateWith(2, 0)
       const output = resetPassedIfWorkshops(state.G, state.ctx)
       expect(output.passed['0']).toBeTruthy()
       expect(output.passed['1']).toBeTruthy()
+    })
+    it('only player 0 has not passed', () => {
+      const state = stateWith(2, 0)
+      const G = {
+        ...state.G,
+        players: {
+          ...state.G.players,
+          0: {
+            ...state.G.players['0'],
+            land: [[{ type: 'novicesHut', contents: [] }]],
+          },
+        },
+      }
+      const output = resetPassedIfWorkshops(G, state.ctx)
+      expect(output.passed['0']).toBeFalsy()
+      expect(output.passed['1']).toBeTruthy()
+    })
+    it('only player 1 has not passed', () => {
+      const state = stateWith(2, 0)
+      const G = {
+        ...state.G,
+        players: {
+          ...state.G.players,
+          1: {
+            ...state.G.players['1'],
+            land: [[{ type: 'plowMakersWorkshop', contents: [] }]],
+          },
+        },
+      }
+      const output = resetPassedIfWorkshops(G, state.ctx)
+      expect(output.passed['0']).toBeTruthy()
+      expect(output.passed['1']).toBeFalsy()
+    })
+    it('both players have not passed', () => {
+      const state = stateWith(2, 0)
+      const G = {
+        ...state.G,
+        players: {
+          ...state.G.players,
+          0: {
+            ...state.G.players['0'],
+            land: [[{ type: 'farmersHouse', contents: [] }]],
+          },
+          1: {
+            ...state.G.players['1'],
+            land: [[{ type: 'workshop', contents: [] }]],
+          },
+        },
+      }
+      const output = resetPassedIfWorkshops(G, state.ctx)
+      expect(output.passed['0']).toBeFalsy()
+      expect(output.passed['1']).toBeFalsy()
     })
   })
 })
